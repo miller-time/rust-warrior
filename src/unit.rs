f3830a733d722575de29b38cc18b2be87f42e4df
Russell Cousineau <miller.time.baby@gmail.com>
2019-08-19 18:17:55 -0700

    sort unit types by alpha

--- src/unit.rs
+++ src/unit.rs
@@ -5,22 +5,22 @@ use crate::Direction;
 /// The Warrior (our protagonist), enemy Sludges and Archers, and Captives.
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum UnitType {
-    Warrior,
-    Sludge,
-    ThickSludge,
     Archer,
     Captive,
+    Sludge,
+    ThickSludge,
+    Warrior,
 }
 
 impl UnitType {
     /// A character (`&str` for convenience) representation of the unit type
     pub fn draw(self) -> &'static str {
         match self {
-            UnitType::Warrior => "@",
-            UnitType::Sludge => "s",
-            UnitType::ThickSludge => "S",
             UnitType::Archer => "a",
             UnitType::Captive => "C",
+            UnitType::Sludge => "s",
+            UnitType::ThickSludge => "S",
+            UnitType::Warrior => "@",
         }
     }
 }
@@ -39,22 +39,33 @@ impl Unit {
     /// Create a unit of type `unit_type` at `position`.
     pub fn new(unit_type: UnitType, position: (i32, i32)) -> Unit {
         match unit_type {
-            UnitType::Warrior => Unit::warrior(position),
-            UnitType::Sludge => Unit::sludge(position),
-            UnitType::ThickSludge => Unit::thick_sludge(position),
             UnitType::Archer => Unit::archer(position),
             UnitType::Captive => Unit::captive(position),
+            UnitType::Sludge => Unit::sludge(position),
+            UnitType::ThickSludge => Unit::thick_sludge(position),
+            UnitType::Warrior => Unit::warrior(position),
         }
     }
 
-    /// Create a unit of type Warrior (20 HP, 5 ATK) at `position`.
-    pub fn warrior(position: (i32, i32)) -> Unit {
+    /// Create a unit of type Archer (7 HP, 3 ATK) at `position`.
+    pub fn archer(position: (i32, i32)) -> Unit {
         Unit {
-            unit_type: UnitType::Warrior,
+            unit_type: UnitType::Archer,
             position,
-            hp: (20, 20),
-            atk: 5,
-            facing: Some(Direction::Forward),
+            hp: (7, 7),
+            atk: 3,
+            facing: None,
+        }
+    }
+
+    /// Create a unit of type Captive (1 HP, 0 ATK) at `position`.
+    pub fn captive(position: (i32, i32)) -> Unit {
+        Unit {
+            unit_type: UnitType::Captive,
+            position,
+            hp: (1, 1),
+            atk: 0,
+            facing: None,
         }
     }
 
@@ -80,25 +91,14 @@ impl Unit {
         }
     }
 
-    /// Create a unit of type Archer (7 HP, 3 ATK) at `position`.
-    pub fn archer(position: (i32, i32)) -> Unit {
+    /// Create a unit of type Warrior (20 HP, 5 ATK) at `position`.
+    pub fn warrior(position: (i32, i32)) -> Unit {
         Unit {
-            unit_type: UnitType::Archer,
+            unit_type: UnitType::Warrior,
             position,
-            hp: (7, 7),
-            atk: 3,
-            facing: None,
-        }
-    }
-
-    /// Create a unit of type Captive (1 HP, 0 ATK) at `position`.
-    pub fn captive(position: (i32, i32)) -> Unit {
-        Unit {
-            unit_type: UnitType::Captive,
-            position,
-            hp: (1, 1),
-            atk: 0,
-            facing: None,
+            hp: (20, 20),
+            atk: 5,
+            facing: Some(Direction::Forward),
         }
     }
 }
