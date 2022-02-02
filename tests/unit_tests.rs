32ce9617939f4f94f4b4b8212a0c04885bb79ee9
Russell <miller.time.baby@gmail.com>
2022-02-02 12:57:57 -0800

    add tests for `UnitType`
    progresses #6

---
+++ tests/unit_tests.rs
@@ -0,0 +1,22 @@
+use rust_warrior::{UnitType};
+
+#[test]
+fn test_unit_type_draw() {
+    let u = UnitType::Archer;
+    assert_eq!(u.draw(), "a");
+
+    let u = UnitType::Captive;
+    assert_eq!(u.draw(), "C");
+
+    let u = UnitType::Sludge;
+    assert_eq!(u.draw(), "s");
+
+    let u = UnitType::ThickSludge;
+    assert_eq!(u.draw(), "S");
+
+    let u = UnitType::Warrior;
+    assert_eq!(u.draw(), "@");
+
+    let u = UnitType::Wizard;
+    assert_eq!(u.draw(), "w");
+}
